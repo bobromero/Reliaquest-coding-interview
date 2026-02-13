d516f10033758e105bb2a33f61b7db070f613a32
bobromero <rromero123102@gmail.com>
2026-02-13 12:58:03 -0500

    added class the implements employee interface

---
+++ api/src/main/java/com/challenge/api/model/EmployeeObject.java
@@ -0,0 +1,23 @@
+package com.challenge.api.model;
+
+import java.time.Instant;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class EmployeeObject implements Employee {
+    private UUID uuid;
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private Integer salary;
+    private Integer age;
+    private String jobTitle;
+    private String email;
+    private Instant contractHireDate;
+    private Instant contractTerminationDate;
+}
